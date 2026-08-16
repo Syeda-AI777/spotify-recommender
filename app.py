1b0699738f491647db1198086cd9b57a2940f792
Syeda-AI777 <mmohathashma@gmail.com>
2026-08-16 19:51:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,28 +1,19 @@
 import streamlit as st
 import pandas as pd
-import joblib
+import pickle
+import gdown
+import os
 
-# Load files
-@st.cache_resource
-def load_data():
-    df = pd.read_pickle("df.pkl")
-    model = joblib.load("spotify_model.pkl")
-    scaler = joblib.load("scaler.pkl")
-    return df, model, scaler
+DF_FILE = "df.pkl"
+DF_URL = "https://drive.google.com/uc?id=17lzURcBBOUlN47hny_E1hwZxyo6DLxP3"
 
-df, model, scaler = load_data()
+if not os.path.exists(DF_FILE):
+    st.info("Downloading data file... please wait 10 seconds")
+    gdown.download(DF_URL, DF_FILE, quiet=False)
 
-st.title("🎵 Spotify Song Recommender")
-st.write("Pick a song and get similar recommendations!")
+df = pd.read_pickle(DF_FILE)
+scaler = pickle.load(open('scaler.pkl', 'rb'))
+model = pickle.load(open('spotify_model.pkl', 'rb'))
 
-song_name = st.selectbox("Choose a song:", df['name'].values)
-
-if st.button("Recommend"):
-    # This part depends on how your model was trained
-    # Adjust column names if yours are different
-    idx = df[df['name'] == song_name].index[0]
-    distances, indices = model.kneighbors(scaler.transform(df.iloc[idx:idx+1].drop(['name','artists'], axis=1)))
-    
-    st.subheader("You might also like:")
-    for i in indices[0][1:6]:
-        st.write(f"- {df.iloc[i]['name']} by {df.iloc[i]['artists']}")
+st.title("Spotify Music Recommender 🎵")
+st.success("App loaded successfully!")
