ec6a41cc1ae93910af219337d6c2702194c7b343
Syeda-AI777 <mmohathashma@gmail.com>
2026-08-16 20:03:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,11 +7,17 @@ import os
 DF_FILE = "df.pkl"
 DF_URL = "https://drive.google.com/uc?id=17lzURcBBOUlN47hny_E1hwZxyo6DLxP3"
 
-if not os.path.exists(DF_FILE):
-    st.info("Downloading data file... please wait 10 seconds")
-    gdown.download(DF_URL, DF_FILE, quiet=False)
+# Force delete old corrupted file and re-download
+if os.path.exists(DF_FILE):
+    os.remove(DF_FILE)
 
-df = pd.read_pickle(DF_FILE)
+st.info("Downloading data file... please wait 10 seconds")
+gdown.download(DF_URL, DF_FILE, quiet=False)
+
+# Load files
+with open(DF_FILE, 'rb') as f:
+    df = pickle.load(f)
+    
 scaler = pickle.load(open('scaler.pkl', 'rb'))
 model = pickle.load(open('spotify_model.pkl', 'rb'))
 
